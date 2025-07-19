dc9a11f92601380e5f86f768f9cef4bd11b42306
luitvm <38038189+eltee1@users.noreply.github.com>
2025-07-19 09:20:06 +0200

    new habitat_areas import files, used to generate new import-files for monitor and calculator.

--- source/modules/src/data/sql/database-modules/grid/dataset_25.sql
+++ source/modules/src/data/sql/database-modules/grid/dataset_25.sql
@@ -1,4 +1,4 @@
-BEGIN; SELECT system.load_table('receptors', '{data_folder}/common/grid/25/grid.receptors_20250519.txt'); COMMIT;
-BEGIN; SELECT system.load_table('hexagons', '{data_folder}/common/grid/25/grid.hexagons_20250519.txt'); COMMIT;
+BEGIN; SELECT system.load_table('receptors', '{data_folder}/common/grid/25/grid.receptors_20250719.txt'); COMMIT;
+BEGIN; SELECT system.load_table('hexagons', '{data_folder}/common/grid/25/grid.hexagons_20250719.txt'); COMMIT;
 
-BEGIN; SELECT system.load_table('geometry_of_interests', '{data_folder}/common/grid/25/grid.geometry_of_interests_20250519.txt'); COMMIT;
+BEGIN; SELECT system.load_table('geometry_of_interests', '{data_folder}/common/grid/25/grid.geometry_of_interests_20250719.txt'); COMMIT;
